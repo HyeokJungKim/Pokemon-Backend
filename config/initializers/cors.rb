03178e7b4c02981377e7368c94cb08ed276275e9
Eric Kim <hyeokjungkim@gmail.com>
2021-03-22 00:22:59 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ['https://pokemon-stay.netlify.app', "http://localhost:3000"]
 
     resource '*',
       headers: :any,
